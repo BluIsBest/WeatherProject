140889fc8dcfd8a131cea31d58b10900e3e7f18f
Joshua Bauer <jbauer4@stevens.edu>
2025-04-12 00:48:22 -0400

    Update app_welcome.py
    Removed first app.layout variable

--- app_welcome.py
+++ app_welcome.py
@@ -7,12 +7,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
 
 # App layout, containing title for app, a secondary title prompting to choose a region, and buttons that will take user to page with dropdown menu
-app.layout = [html.Div(className='row', children='Welcome to Weather Risk Assessment!', style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
-              html.Div(className='row', children='Choose a region:', style={'textAlign': 'center', 'color': 'black', 'fontSize': 24}),
-              dcc.Link(html.Button(id='region1', children='New York'), href="/pages/buoy_list_new_york.py"),
-              dash.page_container
-              ]
-
 app.layout = html.Div([
     html.H1(className='row', children='Welcome to Weather Risk Assessment!', style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
     html.Div([
