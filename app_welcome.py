575c69660105e1e68d3fb3656dea0bfcde5da51a
Joshua Bauer <jbauer4@stevens.edu>
2025-04-12 01:33:00 -0400

    Update app_welcome.py
    Correcting button link initialization

--- app_welcome.py
+++ app_welcome.py
@@ -1,6 +1,6 @@
 # Import packages
 import dash
-from dash import Dash, html, dcc, page_registry, page_container
+from dash import Dash, html, dcc
 
 # Initialize the app and css
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -9,11 +9,12 @@ app = Dash(__name__, use_pages=True, external_stylesheets=external_stylesheets)
 # App layout, containing title for app, a secondary title prompting to choose a region, and buttons that will take user to page with dropdown menu
 app.layout = html.Div([
     html.H1(className='row', children='Welcome to Weather Risk Assessment!', style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+    html.H2(className='row', children='Choose a Region in the United States:', style={'textAlign': 'left', 'color': 'black', 'fontSize': 24}),
     html.Div([
-        html.Div(dcc.Link(html.Button(id='region1', children='New York'), href="/pages/buoy_list_new_york.py")
-                 ) for page in page_registry.values()
+        html.Div(dcc.Link(html.Button(page['name']), href=page['path'])
+                 ) for page in dash.page_registry.values()
     ]),
-    page_container
+    dash.page_container
 ])
 
 #@callback()
