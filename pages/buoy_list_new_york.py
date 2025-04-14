b84613401b11dddbc356d3ab3dd43c363a5e69a2
Joshua Bauer <jbauer4@stevens.edu>
2025-04-13 23:03:16 -0400

    Update buoy_list_new_york.py

--- pages/buoy_list_new_york.py
+++ pages/buoy_list_new_york.py
@@ -6,5 +6,5 @@ dash.page_registry(__name__, name='New York', path='/new-york')
 # dropdown menu of buoys in selected region
 layout = [html.Div(className='row', children='New York Region Buoy Selection',
                    style={'textAlign': 'left', 'color': 'black', 'fontSize': 24}),
-          dcc.Dropdown(['Kings Point, NY','The Battery, NY','Robbins Reef, NJ', 'Mariners Harbor, NY', 'Sandy Hook, NJ', 'Breezy Point, NY'],
-                       'Kings Point, NY', id='dropdown')]
+          dcc.Dropdown(['Islip, NY','Breezy Point, NY','Sandy Hook, NJ','Kings Point, NY','Mariners Harbor, NY','Robbins Reef, NJ','The Battery, NY'],
+                       'Islip, NY', id='dropdown')]
