8deb532bd778ebf039e6e7614f46836f1f96c2d2
Joshua Bauer <jbauer4@stevens.edu>
2025-04-23 16:24:11 -0400

    Add files via upload

--- pages/buoy_list_new_york.py
+++ pages/buoy_list_new_york.py
@@ -1,10 +1,19 @@
-import dash
-from dash import html, dcc
+import dash, gatheringInfo
+from dash import Dash, html, dcc, callback, Input, Output, dash_table
 
-dash.page_registry(__name__, name='New York', path='/new-york')
+dash.register_page(__name__, name='New York', path='/new-york')
+
+app = Dash()
 
 # dropdown menu of buoys in selected region
 layout = [html.Div(className='row', children='New York Region Buoy Selection',
                    style={'textAlign': 'left', 'color': 'black', 'fontSize': 24}),
           dcc.Dropdown(['Islip, NY','Breezy Point, NY','Sandy Hook, NJ','Kings Point, NY','Mariners Harbor, NY','Robbins Reef, NJ','The Battery, NY'],
                        'Islip, NY', id='dropdown')]
+
+@callback(
+    Output(component_id='', component_property='children'),
+    Input(component_id=layout[1],component_property='value')
+)
+def display_data(selected_string):
+    app.layout = dash_table.DataTable({'Data Type': 'Wind Speed','Value': gatheringInfo.gatherWindSpeed(layout[2])})
