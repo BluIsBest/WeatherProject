a47b0e528dea08e97dc87ba7b522735958430dc1
Joshua Bauer <jbauer4@stevens.edu>
2025-04-23 16:27:24 -0400

    Update buoy_list_new_york.py
    See comments

--- pages/buoy_list_new_york.py
+++ pages/buoy_list_new_york.py
@@ -11,9 +11,11 @@ layout = [html.Div(className='row', children='New York Region Buoy Selection',
           dcc.Dropdown(['Islip, NY','Breezy Point, NY','Sandy Hook, NJ','Kings Point, NY','Mariners Harbor, NY','Robbins Reef, NJ','The Battery, NY'],
                        'Islip, NY', id='dropdown')]
 
+# callback decorator allows a table of the weater data to be displayed based on the selection from the dropdown menu.
+# callback Output still needs a component_id string. 
 @callback(
     Output(component_id='', component_property='children'),
     Input(component_id=layout[1],component_property='value')
 )
-def display_data(selected_string):
+def display_data(selected_string): # attempting to create table by making a dictionary in gui code
     app.layout = dash_table.DataTable({'Data Type': 'Wind Speed','Value': gatheringInfo.gatherWindSpeed(layout[2])})
