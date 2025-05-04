64e74d4f03b9852ac5a4f3b175280881a944f226
Joshua Bauer <jbauer4@stevens.edu>
2025-05-04 15:08:43 -0400

    Update maryland.py
    removed prevent_initial_callback for button functionality

--- pages/maryland.py
+++ pages/maryland.py
@@ -15,10 +15,8 @@ layout = html.Div([
                   ])
 
 @callback(
-    Output(component_id='ga_output', component_property='children', allow_duplicate=True),
-    Input(component_id='ga_dropdown', component_property='value'),
-    prevent_initial_call='initial_duplicate'
-
+    Output(component_id='md_output', component_property='children'),
+    Input(component_id='md_dropdown', component_property='value')
 )
 def display_single_buoy_data(selected_string):
     """
